93e1059bbcceb335667e0337550b5cc4cfe5e13b
lordgrenville <joebobson87@yahoo.com>
2017-04-06 16:21:55 +0300

    yotam changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#this file takes in the starting arguments for accessing the redis database.
+# this file takes in the starting arguments for accessing the redis database.
 # it parses them, and creates an object ('args') with these properties
-#it then creates another object, 'myfunc', which is identical except that it also has a printargs function
+# it then creates another object, 'myfunc', which is identical except that it also has a printargs function
 # it then starts the flask app (the UI)
 import re
 import argparse
@@ -8,14 +8,15 @@ from flask import *
 from tools.forms import PrimaryForm, BidForm, CapForm, StatusForm
 from tools.functionality import Functionality, userInfo
 
-#parsing the init arguments for access to the redisDB
+
+# parsing the init arguments for access to the redisDB
 def parsing():
     parser = argparse.ArgumentParser(description='run the app')
-    parser.add_argument('-cr','--crdb', dest="crdb", type=str)
+    parser.add_argument('-cr', '--crdb', dest="crdb", type=str)
     parser.add_argument('-dr', '--drdb', dest="drdb", type=str)
-    parser.add_argument('-redind','--redisindex', dest="redisindex", type=str)
-    parser.add_argument('-redmast','--redismastername', dest="redismastername", type=str)
-    parser.add_argument('-redip','--redisipvec', dest="redisipvec", type=str)
+    parser.add_argument('-redind', '--redisindex', dest="redisindex", type=str)
+    parser.add_argument('-redmast', '--redismastername', dest="redismastername", type=str)
+    parser.add_argument('-redip', '--redisipvec', dest="redisipvec", type=str)
     parser.add_argument('-esip', '--esippush', dest="esippush", type=str)
     parser.add_argument('-esind', '--esindnpush', dest="esindnpush", type=str)
     parser.add_argument('-tz', '--timezone', dest="timezone", type=str)
@@ -23,19 +24,20 @@ def parsing():
     args = parser.parse_args()
     return args
 
-#this method initialises the flask app
-def get_app(myfunc,new_update,config_filename):
+
+# this method initialises the flask app
+def get_app(myfunc, new_update, config_filename):
     app = Flask(__name__)
     # app.config.from_object(config_filename)
     app.config['SECRET_KEY'] = 'bx85EMx91xbf~8x8bxb3xfc!xe1xedxb6*xdeMxd7xeax06x9exda_'
 
-    #home view
+    # home view
     @app.route('/')
     def home():
         form = PrimaryForm(request.form)
         return render_template('base_home.html', form=form)
 
-    #second view - the update view. this is one of three forms
+    # second view - the update view. this is one of three forms
     @app.route('/update', methods=['GET', 'POST'])
     def update():
         form = PrimaryForm(request.form)
@@ -43,7 +45,7 @@ def get_app(myfunc,new_update,config_filename):
         form3 = CapForm(request.form)
         form4 = BidForm(request.form)
 
-        #if the form validates, go to one of three update views
+        # if the form validates, go to one of three update views
         if request.method == 'POST' and form.validate():
             flash('Your campaign ID is valid.')
             choice_name = dict(form.choice.choices).get(form.choice.data)
@@ -61,15 +63,15 @@ def get_app(myfunc,new_update,config_filename):
                 return render_template("bid_home.html", form=form4, choice=choice_name, DSP=new_update.DSP,
                                        cam_id=new_update.campaign_id)
 
-        #if all else fails, go to the beginning view
+        # if all else fails, go to the beginning view
         else:
             return render_template('base_home.html', form=form)
 
-    #third view is result - we will tell the user if the Db has been successfully updated or not
+    # third view is result - we will tell the user if the Db has been successfully updated or not
     @app.route('/result', methods=['GET', 'POST'])
     def result():
 
-        #initialise forms
+        # initialise forms
         form = PrimaryForm(request.form)
         form2 = StatusForm(request.form)
         form3 = CapForm(request.form)
@@ -77,12 +79,13 @@ def get_app(myfunc,new_update,config_filename):
 
         if request.method == 'POST':
 
-            #we need to search old_redis for the correct dict, and then the correct campaign_id, and then alter it
+            # we need to search old_redis for the correct dict, and then the correct campaign_id, and then alter it
             old_redis = myfunc.getdoc(new_update.DSP)
 
-            #handle invalid input
+            # handle invalid input
             if not form3.validate() or not form4.validate():
-                flash('Sorry, your response wasn\'t valid. Make sure that your bid or cap are in number form only (e.g.: 3.15 or 2). Please begin the process again')
+                flash(
+                    'Sorry, your response wasn\'t valid. Make sure that your bid or cap are in number form only (e.g.: 3.15 or 2). Please begin the process again')
                 return render_template("base_home.html", form=form)
 
             else:
@@ -100,22 +103,18 @@ def get_app(myfunc,new_update,config_filename):
                     new_update.get_number(form4.data['bid'])
                     old_redis[new_update.choice][new_update.campaign_id] = new_update.bid
 
-            #upload redis to server, get response, and based on that give either success or failure
-                try:
-                    myfunc.set_doc_by_dsp(new_update.DSP, old_redis)
-                    return render_template("algo_response.html", term="not successful",
-                                           dog="/static/images/sad-dog.jpg")
-                except SystemExit:
-                    return render_template("algo_response.html", term="successful", dog="/static/images/happy-dog2.jpg",
+                return render_template("algo_response.html", term="successful", dog="/static/images/happy-dog2.jpg",
                                        choice=new_update.choicename, id=new_update.campaign_id,
                                        bid=old_redis[new_update.choice][new_update.campaign_id],
                                        redis=old_redis[new_update.choice])
+
     app.debug = False
     return app
 
+
 if __name__ == '__main__':
 
-    #create an object on the server and parse the args
+    # create an object on the server and parse the args
     args = parsing()
 
     crdb = args.crdb
@@ -133,11 +132,11 @@ if __name__ == '__main__':
     timezone = args.timezone
     log_level = args.log_level
 
-    #instantiate two objects
+    # instantiate two objects
     myfunc = Functionality(crdb, drdb, redisindex, redismastername,
                            redisipvec, esippush, esindnpush, timezone, log_level)
 
     new_update = userInfo()
 
-    app = get_app(myfunc,new_update,"settings")
+    app = get_app(myfunc, new_update, "settings")
     app.run()
